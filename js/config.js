9a48807738af2d3893b9a08740cf464aadc0e351
Miguel Sanches <sanches.mi03@hotmail.com>
2026-09-18 19:34:15 +0100

    fix: remove local dynamic stuff

--- js/config.js
+++
@@ -1,5 +0,0 @@
-window.SINFO_CONFIG = {
-    // Company logo, presentation and workshop submission deadline.
-    // Set to null until the deadline is confirmed for the next edition.
-    materialsDeadline: 'December 11th'
-};
